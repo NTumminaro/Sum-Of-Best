238fda948f4a9a54205aeb9ce260900b1c6ff0aa
TonyWorldChanger <114870695+TonyWorldChanger@users.noreply.github.com>
2023-03-13 20:31:54 -0500

    home routes edit

---
+++ controllers/routes - pw hashing here/home-routes.js
@@ -0,0 +1,79 @@
+const router = require('express').Router();
+const { User , Games } = require('../../models');
+
+// GET all games for homepage
+router.get('/', async (req, res) => {
+  try {
+    const dbGamespageData = await Games.findAll({
+      include: [
+        {
+          model: Games,
+          attributes: ['filename', 'description'],
+        },
+      ],
+    });
+
+    const games = dbGamespageData.map((games) =>
+      games.get({ plain: true })
+    );
+    res.render('games', {
+      games,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET game individual page
+router.get('/games/:id', async (req, res) => {
+  try {
+    const dbGamesData = await Games.findByPk(req.params.id, {
+      include: [
+        {
+          model: Games,
+          attributes: [
+            'gameid',
+            'game_name',
+            'release_year',
+            'console_type',
+            'image_url',
+            'discord_url',
+            'notes',
+          ],
+        },
+      ],
+    });
+
+    const games = dbGamesData.get({ plain: true });
+    res.render('games', { games, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET one game
+router.get('/games/:id', async (req, res) => {
+  try {
+    const dbGamesData = await Games.findByPk(req.params.id);
+
+    const game = dbGamesData.get({ plain: true });
+    res.render('games', { game, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// Login route
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
+});
+
+module.exports = router;
