23e7af5ec8524f9f7ef530b573530fef8fba96e2
Tom <tom.miyahira@outlook.com>
2023-03-16 20:14:21 -0700

    minor text updates & removed email form field

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -42,7 +42,7 @@ router.post('/login', async (req, res) => {
     if (!validPassword) {
       res
         .status(400)
-        .json({ message: 'Incorrect username or password. Please try again!' });
+        .json({ message: 'Incorrect email or password. Please try again!' });
       return;
     }
 
