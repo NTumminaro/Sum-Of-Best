446801d185233548cc9b98432fb8b0c3462b319d
NTumminaro <nicholastumminaro@gmail.com>
2023-03-16 20:31:40 -0500

    fixes user routes, login, logout

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const homeRoutes = require('./homeRoutes');
+const users = require('./userRoutes');
 
 router.use('/', homeRoutes);
+router.use('/signup', users);
 
 module.exports = router;
